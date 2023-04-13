7b9b03fd632c0302169b8229655ee4e8490c791e
AngelEC6 <angelestradachavez837@gmail.com>
2023-04-13 17:35:29 -0500

    Adding menu

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-print("Hello World")
-number = 1
-print(number)
+def menu():
+    print("=============================================================")
+    next_menu = input("\n\n\n\n           Are you a user or an administrator: ")
+    return next_menu
+    
+
+
+while True:
+    next_menu = menu()
+    cls
+    if next_menu == "administrator":
+        print("=============================================================")
+        admin_username = input("\n\n\n\n             Username: ")
+        if type(admin_username) == str:
+            break
+    elif next_menu == "administrator":
+        print("=============================================================")
+        user_username = input("\n\n\n\n             Username: ")
+        if type(user_username) == str:
+            break
