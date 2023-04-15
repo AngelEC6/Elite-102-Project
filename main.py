11858e6a220837662019b1dbeb30a37ab902d20e
AngelEC6 <angelestradachavez837@gmail.com>
2023-04-15 15:46:59 -0600

    Added password function and existing user

--- main.py
+++ main.py
@@ -7,40 +7,99 @@ connection = mysql.connector.connect(user="root",database = "elite 102 project",
 top_logo = "=============================================================\n                   Ficticious Bank\n============================================================="
 def main_menu():
     print(top_logo)
-    next_menu = input("\n\n\n\nAre you a user or an administrator? : ")
+    next_menu = input("\n\n\n\nAre you a user or an administrator?: ")
     return next_menu
-    
-def user_menu():
+
+def check_user_type():
     print(top_logo)
-    user_id_input = input("\n\n\n\nAcount Number: ")
-    passed = check_id(user_id_input)
-    #os.system('cls')
-    if passed == True:
-        password_menu()
-    elif user_id_input == "Back":
-        print("")
+    user_type_input = input("\n\n\n\nAre you an existing or new user?: ")
+    if user_type_input == "existing":
+        return user_type_input
+    elif user_type_input == "new":
+        return user_type_input
     else:
         while True:
             print(top_logo)
-            user_id_input = input("\n\n\nThat acount doesn't exist.Please try again.\nAcount Number: ")
-            passed = check_id(user_id_input)
-            #os.system('cls')
+            user_type_input = input("\n\n\nThat is not a valid response please try again. This time respond using either new or existing.\nAre you an existing or new user?: ")
+            if user_type_input == "existing":
+                return user_type_input
+            elif user_type_input == "new":
+                return user_type_input
+
+
+def existing_acount_number_menu():
+    while True:
+        try:
+            print(top_logo)
+            user_id_input = input("\n\n\n\nAcount Number: ")
+            if user_id_input == "Back" or user_id_input == "back":
+                return user_id_input
+            user_id_input = int(user_id_input)
+            if type(user_id_input) == int:
+                break
+        except ValueError:
+            print(top_logo)
+            print("\n\n\n\nThat wasn't a number please try again.")
+    passed = check_id(user_id_input)
+    #os.system('cls')
+    if passed == True:
+        password_menu(user_id_input)
+    elif user_id_input == "Back":
+        return null
+    else:
+        while True:
+            try:
+                print(top_logo)
+                user_id_input = int(input("\n\n\nThat acount doesn't exist.Please try again.\nAcount Number: "))
+                passed = check_id(user_id_input)
+                #os.system('cls')
+                if passed == True:
+                    break
+            except ValueError:
+                print(top_logo)
+                print("\n\n\n\nThat wasn't a number please try again.")
+        password_menu(user_id_input)
+    
+def password_menu(acount_number):
+    print(top_logo)
+    user_password_input = input("\n\n\n\nPassword: ")
+    passed = check_password(user_password_input,acount_number)
+    if passed == True:
+        existing_user_menu()
+    else:
+        while True:
+            print(top_logo)
+            user_password_input = input("\n\n\nThat password was incorrect.Please Try again.\nPassword: ")
+            passed = check_password(user_password_input,acount_number)
             if passed == True:
                 break
-        print("tried again")
-    
-def password_menu():
-    print("password menu")
+        existing_user_menu()
 
+def existing_user_menu():
+    print("existing user menu")
+
+    
+def check_password(user_input,acount_number):
+    check_password_cursor = connection.cursor()
+    get_password = ("SELECT userpassword FROM acountinfo WHERE idnumber = " + str(acount_number))
+    check_password_cursor.execute(get_password)
+    password = check_password_cursor.fetchall()
+    if password[0][0] == user_input:
+        check_password_cursor.close()
+        return True
+    else:
+        check_password_cursor.close()
+        return False
+    
 
 def check_id(user_input):
     check_number = connection.cursor()
-    get_number = ("SELECT idnumber FROM acountinfo")
-    check_number.execute(get_number)
-    acount_number = check_number.fetchall()
-    for number in acount_number:
+    get_numbers = ("SELECT idnumber FROM acountinfo")
+    check_number.execute(get_numbers)
+    acount_numbers = check_number.fetchall()    
+    for number in acount_numbers:
         print(number)
-        if number[0] == int(user_input):
+        if number[0] == user_input:
             check_number.close()
             return True
     check_number.close()
@@ -55,7 +114,11 @@ while True:
         if type(admin_username) == str:
             break
     elif next_menu == "user":
-        user_menu()
+        user_type = check_user_type()
+        if user_type == "new":
+            print("new user menu")
+        elif user_type == "existing":
+            existing_acount_number_menu()
     else:
         print(top_logo)
 
