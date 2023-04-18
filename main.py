57f2af99ee0decfd5a29acf2e62808f25a5a912a
AngelEC6 <angelestradachavez837@gmail.com>
2023-04-18 10:43:28 -0500

    New user functions

--- main.py
+++ main.py
@@ -26,6 +26,59 @@ def check_user_type():
             elif user_type_input == "new":
                 return user_type_input
 
+def new_acount_menu():
+    print(top_logo)
+    create_acount = input("\n\n\n\nWould you like to open a new acount?: ")
+    if create_acount == "yes":
+        create_new_acount_menu()
+    elif create_acount == "back":
+        return "null"
+    else:
+        while True:
+            print(top_logo)
+            create_acount = input("\n\n\nThat is not a valid input.Please try again.\nWould you like to open a new acount?: ")
+            if create_acount == "yes":
+                create_new_acount_menu()
+                break
+            elif create_acount == "back":
+                return "null"
+
+def create_new_acount_menu():
+    user_name = get_user_name()
+    pin = get_user_pin()
+    new_acount_info = create_new_acount(user_name,pin)
+    print(top_logo)
+    input("\n\nYour acount has been succesfully created.\nYour acount details are:\nAcount Number: " + str(new_acount_info[0][0]) + "\nPin: " + str(new_acount_info[0][1]) + "\nBalance: " + str(new_acount_info[0][2]) + "\nName: " + new_acount_info[0][3] + "\nPress enter when you're ready to move on.")
+
+def create_new_acount(new_user_name,new_user_pin):
+    create_new_acount_cursor = connection.cursor()
+    create_new_acount_code = ("INSERT INTO acountinfo (pin,amount,username) VALUES ('" + str(new_user_pin) + "', 0, '" + new_user_name + "')")
+    create_new_acount_cursor.execute(create_new_acount_code)
+    create_new_acount_cursor.close()
+    check_new_acount_cursor = connection.cursor()
+    check_new_acount_code = ("SELECT * FROM acountinfo WHERE pin = " + str(new_user_pin))
+    check_new_acount_cursor.execute(check_new_acount_code)
+    new_acount_info = check_new_acount_cursor.fetchall()
+    check_new_acount_cursor.close()
+    return new_acount_info
+
+
+def get_user_name():
+    print(top_logo)
+    new_user_name = input("\n\n\n\nEnter your name: ")
+    return new_user_name
+
+def get_user_pin():
+    while True:
+        try:
+            print(top_logo)
+            new_user_password = int(input("\n\n\n\nEnter your pin: "))
+            break
+        except ValueError:
+            print(top_logo)
+            print("\n\n\n\nThat's not a number please try again.")
+    return new_user_password
+
 
 def existing_acount_number_menu():
     while True:
@@ -43,9 +96,9 @@ def existing_acount_number_menu():
     passed = check_id(user_id_input)
     #os.system('cls')
     if passed == True:
-        password_menu(user_id_input)
+        pin_menu(user_id_input)
     elif user_id_input == "Back":
-        return null
+        return "null"
     else:
         while True:
             try:
@@ -58,37 +111,141 @@ def existing_acount_number_menu():
             except ValueError:
                 print(top_logo)
                 print("\n\n\n\nThat wasn't a number please try again.")
-        password_menu(user_id_input)
+        pin_menu(user_id_input)
     
-def password_menu(acount_number):
-    print(top_logo)
-    user_password_input = input("\n\n\n\nPassword: ")
-    passed = check_password(user_password_input,acount_number)
-    if passed == True:
-        existing_user_menu()
-    else:
-        while True:
+def pin_menu(acount_number):
+    while True:
+        try:
             print(top_logo)
-            user_password_input = input("\n\n\nThat password was incorrect.Please Try again.\nPassword: ")
-            passed = check_password(user_password_input,acount_number)
+            user_pin_input = input("\n\n\n\nPin: ")
+            if user_pin_input == "back":
+                return "null"
+            user_pin_input = int(user_pin_input)
+            passed = check_pin(user_pin_input,acount_number)
             if passed == True:
-                break
-        existing_user_menu()
+                existing_user_menu(acount_number)
+                return "void"
+        except ValueError:
+            print(top_logo)
+            print("\n\n\n\nThat's not a number please try again")
+    while True:
+        try:
+            if passed != True:
+                while True:
+                    print(top_logo)
+                    user_pin_input = input("\n\n\nThat pin was incorrect.Please Try again.\nPin: ")
+                    if user_pin_input == "back":
+                        return "null"
+                    user_pin_input = int(user_pin_input)
+                    passed = check_pin(user_pin_input,acount_number)
+                    if passed == True:
+                        break
+                existing_user_menu(acount_number)
+                return "null"
+        except ValueError:
+            print(top_logo)
+            print("\n\n\n\nThat's not a number please try again.")
 
-def existing_user_menu():
-    print("existing user menu")
+def existing_user_menu(acount_number):
+    while True:
+        print(top_logo)
+        user_choice = input("\n\n\n\nWhat would you like to do (deposit,withdrawal,check balance,)?: ")
+        if user_choice == "deposit":
+            user_deposit_menu(acount_number)
+        elif user_choice == "withdrawal":
+            user_withdrawal_menu(acount_number)
+        elif user_choice == "check balance":
+            user_check_balance_menu(acount_number)
+        elif user_choice == "back":
+            break
+        else:
+            while True:
+                print(top_logo)
+                user_choice = input("\n\n\nSorry that is not a valid option. Please try again.\nWhat would you like to do (deposit,withdrawal,check balance,)?: ")
+                if user_choice == "deposit":
+                    user_deposit_menu(acount_number)
+                    break
+                elif user_choice == "withdrawal":
+                    user_withdrawal_menu(acount_number)
+                    break
+                elif user_choice == "check balance":
+                    user_check_balance_menu(acount_number)
+                    break
+                elif user_choice == "back":
+                    return "null"
+
+def user_deposit_menu(acount_number):
+    while True:
+        try:
+            print(top_logo)
+            user_deposit_input = input("\n\n\n\nHow much money would you like to deposit?: ")
+            if user_deposit_input == "back":
+                return "null"
+            user_deposit_input = float(user_deposit_input)
+            break
+        except ValueError:
+            print(top_logo)
+            print("\n\n\n\nThat is not a valid input. Please try again.")
+    deposit(user_deposit_input,acount_number)
+
+def deposit(amount,acount_number):
+    acount_balance = check_balance(acount_number)
+    new_balance = float(acount_balance) + amount
+    deposit_cursor = connection.cursor()
+    deposit_amount = ("UPDATE acountinfo SET amount = " + str(new_balance) + " WHERE idnumber = " + str(acount_number))
+    deposit_cursor.execute(deposit_amount)
+    deposit_cursor.close()
+
+
+
+def user_withdrawal_menu(acount_number):
+    while True:
+        try:
+            print(top_logo)
+            user_withdrawal_input = input("\n\n\n\nHow much money would you like to withdrawal?: ")
+            if user_withdrawal_input == "back":
+                return "null"
+            user_withdrawal_input = float(user_withdrawal_input)
+            break
+        except ValueError:
+            print(top_logo)
+            print("\n\n\n\nThat is not a valid input. Please try again.")
+    withdrawal(user_withdrawal_input,acount_number)
+
+def withdrawal(amount,acount_number):
+    acount_balance = check_balance(acount_number)
+    new_balance = float(acount_balance) - amount
+    withdrawal_cursor = connection.cursor()
+    withdrawal_amount = ("UPDATE acountinfo SET amount = " + str(new_balance) + " WHERE idnumber = " + str(acount_number))
+    withdrawal_cursor.execute(withdrawal_amount)
+    withdrawal_cursor.close()
+
+def user_check_balance_menu(acount_number):
+    print(top_logo)
+    balance = check_balance(acount_number)
+    print("\n\n\n\nYou current balance is " + str(balance))
+    input("When you would like to go back type 'back': ")
+
+def check_balance(acount_number):
+    check_balance_cursor = connection.cursor()
+    get_acount_balance = ("SELECT amount FROM acountinfo WHERE idnumber = " + str(acount_number))
+    check_balance_cursor.execute(get_acount_balance)
+    acount_balance = check_balance_cursor.fetchall()
+    check_balance_cursor.close()
+    print(type(acount_balance[0][0]))
+    return acount_balance[0][0]
 
     
-def check_password(user_input,acount_number):
-    check_password_cursor = connection.cursor()
-    get_password = ("SELECT userpassword FROM acountinfo WHERE idnumber = " + str(acount_number))
-    check_password_cursor.execute(get_password)
-    password = check_password_cursor.fetchall()
-    if password[0][0] == user_input:
-        check_password_cursor.close()
+def check_pin(user_input,acount_number):
+    check_pin_cursor = connection.cursor()
+    get_pin = ("SELECT pin FROM acountinfo WHERE idnumber = " + str(acount_number))
+    check_pin_cursor.execute(get_pin)
+    pin = check_pin_cursor.fetchall()
+    if pin[0][0] == user_input:
+        check_pin_cursor.close()
         return True
     else:
-        check_password_cursor.close()
+        check_pin_cursor.close()
         return False
     
 
@@ -109,14 +266,11 @@ while True:
     next_menu = main_menu()
     #os.system('cls')
     if next_menu == "administrator":
-        print(top_logo)
-        admin_username = input("\n\n\n\nUsername: ")
-        if type(admin_username) == str:
-            break
+        admin_menu()
     elif next_menu == "user":
         user_type = check_user_type()
         if user_type == "new":
-            print("new user menu")
+            new_acount_menu()
         elif user_type == "existing":
             existing_acount_number_menu()
     else:
